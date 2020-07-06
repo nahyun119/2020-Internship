2e4ef6d043be6a9e449190641eee66d71fb24b0b
KIM NA HYUN <nahyun858@ajou.ac.kr>
2020-07-06 16:25:33 +0900

    db 관계형 이해 & erd

---
+++ express_project/routes/index.js
@@ -0,0 +1,73 @@
+var express = require('express');
+var router = express.Router();
+
+const mysql = require('mysql');
+const dbConfig = require('../config/database')
+const connection = mysql.createConnection(dbConfig);
+
+//connection.connect();
+
+
+/**
+ * @swagger
+ * tags:
+ *    name: Todo
+ *    description : hello express, swagger
+ * definitions:
+ *    Todo:
+ *      type: object
+ *      required:
+ *        - content
+ *      properties:
+ *        _id :
+ *          type: string
+ *          description : ObjectID
+ *        content:
+ *          type: string
+ *          description: 할일 내용
+ *        done:
+ *          type: boolean
+ *          description: 완료 여부
+ *
+ */
+/**
+ * @swagger
+ * /todo :
+ *  get:
+ *    summary : Returns Todo list
+ *    tags : [Todo]
+ *    responses:
+ *      200:
+ *        description: 뇸뇸뇸뇸뇸
+ *        schema :
+ *          type : object
+ *          properties:
+ *            todos:
+ *              type : array
+ *              items :
+ *                $ref : '#/definitions/Todo'
+ *
+ */
+
+router.get('/todo', function(req,res,next){
+  var todo;
+  connection.query('SELECT content from Todo where _id = "first"', (err, rows, field) => {
+    if (err) throw err;
+    todo = rows;
+    res.send(todo);
+  })
+
+})
+
+
+
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
+});
+
+
+
+module.exports = router;
+
